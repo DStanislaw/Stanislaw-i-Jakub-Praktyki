452e8b050c626e6ce22062b4bb5e0df965484bbe
Kejaze <jakub.kamieniczny111@gmail.com>
2024-12-11 13:54:46 +0100

    buttons-naprawa

--- JS/buttons.js
+++ JS/buttons.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const productCards = document.querySelectorAll(".product-card");
   
     productCards.forEach((card) => {
-      const minusBtn = card.querySelector(".quantity-controls button:first-child"); // Przycisk "-"
-      const plusBtn = card.querySelector(".quantity-controls button:last-child"); // Przycisk "+"
-      const quantityInput = card.querySelector(".quantity-controls .quantity"); // Pole ilości
+      const minusBtn = card.querySelector(".product-card__quantity-controls button:first-child"); // Przycisk "-"
+      const plusBtn = card.querySelector(".product-card__quantity-controls button:last-child"); // Przycisk "+"
+      const quantityInput = card.querySelector(".product-card__quantity-controls .product-card__quantity-input"); // Pole ilości
   
       // Obsługa zmniejszania ilości
       minusBtn.addEventListener("click", () => {
